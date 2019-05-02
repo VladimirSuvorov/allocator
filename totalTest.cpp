551a7a125b3541c33a4e5a1ede4f59396e5f02c2
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-05-02 21:44:04 +0300

    Update totalTest.cpp

--- totalTest.cpp
+++ totalTest.cpp
@@ -1,7 +1,6 @@
 #include "TestingCore.hxx"
 
-
-/*TEST(mustAllocateMemoryNotMoreThanOneTimePer_N_elements, mustAllocateMemoryNotMoreThanOneTimePer_N_elements){
+TEST(mustAllocateMemoryNotMoreThanOneTimePer_N_elements, mustAllocateMemoryNotMoreThanOneTimePer_N_elements){
     using namespace std;
     bool mustBeTrue = mustAllocateMemoryNotMoreThanOneTimePer_N_elements<size_t,size_t,10,0>();
     ASSERT_TRUE(mustBeTrue);
@@ -10,14 +9,14 @@ TEST(mustAllocateAndDeallocateMemorySameTimes, mustAllocateAndDeallocateMemorySa
     using namespace std;
     bool mustBeTrue = mustAllocateAndDeallocateMemorySameTimes<size_t,size_t,10,0>();
     ASSERT_TRUE(mustBeTrue);
-}*/
+}
 TEST(memoryBlockMustBeEmptyBeforeAllocationAndDeallocateCorrectly, memoryBlockMustBeEmptyBeforeAllocationAndDeallocateCorrectly){
     using namespace std;
     bool mustBeTrue = memoryBlockMustBeEmptyBeforeAllocationAndDeallocateCorrectly<size_t,size_t,10,0,3>();
     ASSERT_TRUE(mustBeTrue);
 }
-/*TEST(customSListContainerTest, customSListContainerTest){
+TEST(customSListContainerTest, customSListContainerTest){
     using namespace std;
     bool mustBeTrue = customSListContainerTest<size_t,size_t,10,0>();
     ASSERT_TRUE(mustBeTrue);
-}*/
+}
