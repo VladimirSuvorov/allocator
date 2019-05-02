cef8641926348c1f107efb1c0d1f5429451a7436
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-05-02 20:03:34 +0300

    Update totalTest.cpp

--- totalTest.cpp
+++ totalTest.cpp
@@ -6,12 +6,12 @@ TEST(mustAllocateMemoryNotMoreThanOneTimePer_N_elements, mustAllocateMemoryNotMo
     bool mustBeTrue = mustAllocateMemoryNotMoreThanOneTimePer_N_elements<size_t,size_t,10,0>();
     ASSERT_TRUE(mustBeTrue);
 }
-/*TEST(mustAllocateAndDeallocateMemorySameTimes, mustAllocateAndDeallocateMemorySameTimes){
+TEST(mustAllocateAndDeallocateMemorySameTimes, mustAllocateAndDeallocateMemorySameTimes){
     using namespace std;
     bool mustBeTrue = mustAllocateAndDeallocateMemorySameTimes<size_t,size_t,10,0>();
     ASSERT_TRUE(mustBeTrue);
 }
-TEST(memoryBlockMustBeEmptyBeforeAllocationAndDeallocateCorrectly, memoryBlockMustBeEmptyBeforeAllocationAndDeallocateCorrectly){
+/*TEST(memoryBlockMustBeEmptyBeforeAllocationAndDeallocateCorrectly, memoryBlockMustBeEmptyBeforeAllocationAndDeallocateCorrectly){
     using namespace std;
     bool mustBeTrue = memoryBlockMustBeEmptyBeforeAllocationAndDeallocateCorrectly<size_t,size_t,10,0,3>();
     ASSERT_TRUE(mustBeTrue);
