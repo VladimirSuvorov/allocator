bba648d74bf2348132b2a8f75a3cda37eb27a4e0
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-05-02 20:24:30 +0300

    Update NewDeleteCallCounter.hxx

--- NewDeleteCallCounter.hxx
+++ NewDeleteCallCounter.hxx
@@ -1,5 +1,10 @@
 #ifndef ALLOCATOR_PROJECT_NEW_DELETE_CALL_COUNTER_HXX
 #define ALLOCATOR_PROJECT_NEW_DELETE_CALL_COUNTER_HXX
+
+#ifndef ALLOCATOR_PROJECT_NEW_DELETE_CALL_COUNTER_USE_OUTPUT
+#define ALLOCATOR_PROJECT_NEW_DELETE_CALL_COUNTER_DONT_USE_OUTPUT
+#endif
+
 struct {
 	size_t new_calls = 0;
 	size_t delete_calls = 0;
@@ -27,14 +32,18 @@ constexpr R not_return_throw() {
 void* operator new(std::size_t sz) {
 	using namespace std;
 	global_new_delete_counters.on_new_is_called();
+#ifndef ALLOCATOR_PROJECT_NEW_DELETE_CALL_COUNTER_DONT_USE_OUTPUT	
 	printf("%zu) allocation performed, size = %zu\n", global_new_delete_counters.new_calls, sz);
+#endif
 	auto ptr = malloc(sz);
 	return (ptr) ? ptr : not_return_throw<void*, std::bad_alloc>();
 }
 void operator delete(void* ptr) noexcept {
 	using namespace std;
-	global_new_delete_counters.on_delete_is_called();
+	global_new_delete_counters.on_delete_is_called();	
+#ifndef ALLOCATOR_PROJECT_NEW_DELETE_CALL_COUNTER_DONT_USE_OUTPUT	
 	printf("%zu) deallocation performed\n", global_new_delete_counters.delete_calls);
+#endif
 	free(ptr);
 }
 
